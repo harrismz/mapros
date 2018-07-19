7d8aa27b2002076bfd23077ad2ecac7b1581956c
Hakim <alfiandi.hakim@jvc-jein.co.id>
2018-07-19 13:47:38 +0700

    adding FormInput

---
+++ app/view/main/ListController.js
@@ -0,0 +1,38 @@
+/**
+ * This class is the controller for the main view for the application. It is specified as
+ * the "controller" of the Main view class.
+ *
+ * TODO - Replace this content of this view to suite the needs of your application.
+ */
+Ext.define('mapros.view.main.ListController', {
+    extend: 'Ext.app.ViewController',
+
+    alias: 'controller.list',
+
+    addOnClick: function() {
+      console.log('terpanggil')
+      let modal = Ext.create('Ext.window.Window',{
+        title: 'Form Input',
+        width: 400,
+        height: 200,
+        layout: 'fit',
+        items:[
+          {xtype: 'forminput'}
+        ]
+      });
+
+      modal.show();
+    }
+
+    // onItemSelected: function (sender, record) {
+    //     Ext.Msg.confirm('Confirm', 'Are you sure?', 'onConfirm', this);
+    // },
+    //
+    // onConfirm: function (choice) {
+    //     if (choice === 'yes') {
+    //         Ext.Msg.alert("Alert","Harris bau !");
+    //     } else {
+    //       Ext.Msg.alert("Alert","Harris ganteng !");
+    //     }
+    // }
+});
